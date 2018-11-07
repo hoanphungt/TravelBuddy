4a53d83058e8d63e0e2d6769c51f3f72fbe1160a
Hoan Phung <43402556+hoanphungt@users.noreply.github.com>
2018-11-07 16:40:42 +0100

    Update App.js
    Add routes to Matching list and match person pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import About from './components/About'
 import Contact from './components/Contact'
 import NewTravel from './components/NewTravel'
 import FindMatch from './components/FindMatch'
+import MatchingList from './components/MatchingList';
+import MatchPerson from './components/MatchPerson';
 
 class App extends Component {
   render() {
@@ -17,7 +19,9 @@ class App extends Component {
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
           <Route path='/newtravel' component={NewTravel} />          
-          <Route path='/findmatch' component={FindMatch} />          
+          <Route path='/findmatch' component={FindMatch} />
+          <Route path='/matchinglist' component={MatchingList}/>
+          <Route exact path="/matches/:id" component={MatchPerson} />
         </div>
       </BrowserRouter>
     );
