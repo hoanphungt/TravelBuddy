7084dbc4e22131ef8f538da810bc54b22591d3b1
DavidB59 <david.behal@net-c.com>
2018-11-08 00:05:13 +0100

    avatarworking2

--- src/components/MatchPerson.js
+++ src/components/MatchPerson.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class MatchPerson extends Component {
+/*  state = {
 
+  }
+
+          <img src={require(`${srcString}`)}alt ='avatar'/>
+
+  sourcePhoto = () => {
+    this.setState({
+      scrString :this.props.travelers.filter(traveler => traveler.id == id)[0].photo
+    })
+  }*/
   render() {
     const id = this.props.match.params.id
-    
+
+    const srcString = this.props.travelers.filter(traveler => traveler.id == id)[0].photo
+    console.log(srcString)
     return (
       <>
+      {this.sourcePhoto}
         <div>
           <strong>{this.props.travelers.filter(traveler => traveler.id == id)[0].firstName} {this.props.travelers.filter(traveler => traveler.id == id)[0].lastName}</strong>
 
@@ -18,6 +31,9 @@ class MatchPerson extends Component {
         </div>
         <div>
           <strong>Feedback:</strong> {this.props.travelers.filter(traveler => traveler.id == id)[0].feedback}
+          {srcString ?           <img src={require(`${srcString}`)}alt ='avatar'/>
+: <h1>No Avatar</h1>}
+
 
         </div>         
       </>
