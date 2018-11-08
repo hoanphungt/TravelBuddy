cbc8734d6214d11e5c28d117ebff4006ed45fec4
DavidB59 <david.behal@net-c.com>
2018-11-08 11:27:20 +0100

    Navbar with username

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = (props) => {
 
   const string = store.getState()[1].photo
   const signedUp = store.getState()[1]
-  const  userName = store.getState()[1].firstname
+  const  userName = store.getState()[1].firstName
 
   return (
     <nav className="nav-wrapper red darken-3">
@@ -14,7 +14,7 @@ const Navbar = (props) => {
       {string ? <img  src={require(`${string}`)} alt='avatar'/> : <img  src={require(`./images/happy2.svg`)} alt='avatar'/>}
         <a className="brand-logo">Travel Buddy</a>
         <ul className="right">
-        <li>{signedUp ? <h4>test</h4>  : <h4> Sign Up </h4>}</li>
+        <li>{signedUp ? <h4>{userName}</h4>  : <h4> Sign Up </h4>}</li>
           <li><NavLink to="/home">Home</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/contact">Contact</NavLink></li>
