05bfe1cc079f3f5c2283744ca24ddf6e8a7c43dd
electrologos <electrologosduth@hotmail.com>
2018-11-08 12:32:13 +0100

    NewTraveler

--- src/components/NewTravel.js
+++ src/components/NewTravel.js
@@ -29,28 +29,33 @@ export default class NewTravel extends Component {
 
   render() {
     return (
-      <div>
-        <form>
-          <label>
-            location :
-            <input type="text" name="name"  onChange={this.defineLocation}  />
-          </label>
-        <div>
-          <label>
-            Arrival  :
-            <input type="text" name="name" onChange={this.defineArrivalDate} />
-          </label>
+      <div className="container">
+        <form className="center">
+          <h4>I'm going to</h4>
+          <div className="row">
+            <div className="input-field col s6 offset-s3">
+              <input id="destination" placeholder="Try Berlin..." type="text" onChange={this.defineLocation}/>
+              <label for="destination"></label>
+            </div>
+          </div>
+          <h4>On the following period</h4>
+          <div className="row">
+            <div className="input-field col s2 offset-s3">
+              <input id="startDate" type="text" onChange={this.defineArrivalDate}/>
+              <label for="startDate"></label>
+            </div>
+            <div className='col s2'>
+              <h4 className="center">to</h4>
+            </div>
+            <div className="input-field col s2">
+              <input id="lastDate" type="text" onChange={this.defineDepartureDate}/>
+              <label for="lastDate"></label>
+            </div>
+          </div>
+        </form>
+        <div className='container'>
+          <Match location ={this.state.location} ></Match>
         </div>
-        <div>
-          <label>
-            Departure  :
-            <input type="text" name="name" onChange={this.defineDepartureDate}  />
-          </label>
-        </div>
-        </form> 
-        <div>
-        </div>
-        <Match location ={this.state.location} ></Match>
       </div>
     );
   }
